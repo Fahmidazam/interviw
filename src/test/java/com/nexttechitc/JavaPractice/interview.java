7d0435701970fa8d5643ac19687b3908dcf5b1c7
fahmida <you@example.com>
2024-02-15 13:38:33 -0600

    initial commit

---
+++ src/test/java/com/nexttechitc/JavaPractice/interview.java
@@ -0,0 +1,10 @@
+package com.nexttechitc.JavaPractice;
+
+public class interview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/interviewparactice/interview.java
@@ -0,0 +1,10 @@
+package interviewparactice;
+
+public class interview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
